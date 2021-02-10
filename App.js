c4e0e59ce3b3737845d53d14366442d64b484e26
Manqoba Dlamini <manqobadlamini@Manqobas-MacBook-Air.local>
2021-02-10 04:10:49 +0200

    Added Google Maps

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from './src/Screens/Main';
+import MapView from './src/Screens/MapView';
 import getWeather from './src/Utils/Weather';
 import Loader from './src/Components/Loader';
 export default function App() {
   const weather = getWeather();
   if (weather) {
-    return <Main forecast={weather} />;
+    // return <Main forecast={weather} />;
+    return <MapView forecast={weather} />;
   } else return <Loader />;
 }
---
+++ src/Screens/MapView/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
+
+export default function Index(props) {
+  console.log('props', props);
+  return (
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}></MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    height: '100%',
+  },
+  bubble: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  arrow: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5,
+  },
+  name: {
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  image: {
+    width: 120,
+    height: 80,
+  },
+});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//     height: 400,
+//     width: 400,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+// });
